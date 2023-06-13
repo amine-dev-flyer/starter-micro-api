ce754e0141976b21be07c46f16c63cd94aeafedc
amine-dev-flyer <136484744+amine-dev-flyer@users.noreply.github.com>
2023-06-13 17:22:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,28 @@
-import http from 'http';
-import fs from "fs"
+import http from "http";
+import fs from "fs";
 
-http.createServer(function (req, res) {
+http
+  .createServer(function (req, res) {
+    var count = 0;
 
-var count = 0;
-
-    
     count++;
     const date = new Date().toJSON();
 
-    console.log('---------------------');
-    console.log('Count :'+count);
-    console.log('Date :'+date);
-    
-    res.write('!!'+date);
-    res.end(); 
-    
-    fs.appendFile(`file-${date}--${count}.json`, JSON.stringify(''), "utf8", function(err) {
-        if (err) throw err;
-        console.log("!!! File saved.");
-      });  
-    
-    }).listen(process.env.PORT || 3000);
+    console.log("---------------------");
+    console.log("Count :" + count);
+    console.log("Date :" + date);
 
-    /*fs.appendFile(`file-${date}--${count}.json`, JSON.stringify(''), "utf8", function(err) {
-        if (err) throw err;
-        console.log("!!! File saved.");
-      }); 
-     
-  });
- */
-  //getQuotes();
-
-
-
-
-/*import http from 'http';
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    const date = new Date().toJSON();
-    res.write('!!'+date);
-    
+    res.write("!!" + date);
     res.end();
-}).listen(process.env.PORT || 3000);
 
-console.log("test");*/
+    fs.appendFile(
+      `file-${date}--${count}.json`,
+      JSON.stringify(""),
+      "utf8",
+      function (err) {
+        if (err) throw err;
+        console.log("!!! File saved.");
+      }
+    );
+  })
+  .listen(process.env.PORT || 3000);
