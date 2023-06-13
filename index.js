7c07f9df6606de58b6cff190d70efa5c69c30cd4
amine-dev-flyer <136484744+amine-dev-flyer@users.noreply.github.com>
2023-06-13 16:33:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
+    res.write('INDEX!');
     res.end();
 }).listen(process.env.PORT || 3000);
+
+console.log("test");
